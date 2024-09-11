df31ccfbb616b5d3b0770a83b9e7d6c2bdee8baa
Dmitryi Lavrentev <Tyuiu.LavrentevDA@outlook.com>
2024-09-11 18:11:12 +0500

    Task1

---
+++ Tyuiu.LavrentevDA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.LavrentevDA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Дима";
+            int age = 21;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " год! ");
+            Console.ReadKey();
+        }
+    }
+}

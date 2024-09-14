7c87a173f8c368e3de7a74af9aaa84d4203630a0
Dmitryi Lavrentev <Tyuiu.LavrentevDA@outlook.com>
2024-09-14 14:19:19 +0500

    Task3

---
+++ Tyuiu.LavrentevDA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.LavrentevDA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.LavrentevDA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}

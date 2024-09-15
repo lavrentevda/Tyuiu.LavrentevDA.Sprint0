e7b606ad33f1ab5310d918540527c26fb94fd973
Dmitryi Lavrentev <Tyuiu.LavrentevDA@outlook.com>
2024-09-15 14:28:00 +0500

    Task6

--- Tyuiu.LavrentevDA.Sprint0.Task5.V0/Program.cs
+++ Tyuiu.LavrentevDA.Sprint0.Task5.V0/Program.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.LavrentevDA.Sprint0.Task5.V0
         {
             Console.WriteLine("A + B = " + DataService.Addition(1, 5));
             Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
-            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10^2, 10));
             Console.WriteLine("A / B = " + DataService.Division(9, 3));
             Console.ReadKey();
         }
